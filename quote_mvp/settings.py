b64929b819727c96b8b90b3f3efd4471c2b621fd
Leonardo <rmalonbazan@gmail.com>
2025-05-28 00:51:58 -0500

    CSRF Corrected

--- quote_mvp/settings.py
+++ quote_mvp/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CSRF_TRUSTED_ORIGINS = ['https://gentle-gratitude-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://cotilink.up.railway.app']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
